fb265482b9764e13f0206b12aea6399555a9dead
Po-Wei Wang <xflash96@gmail.com>
2012-02-19 14:25:36 +0800

    use route

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -7,10 +7,11 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('myapp.views.my_view',
-                    context='myapp:resources.Root',
+    config.add_route('default', '')
+    config.add_view('myapp.views.my_view', 'default'
                     renderer='myapp:templates/mytemplate.pt')
     config.add_static_view('static', 'myapp:static', cache_max_age=3600)
+
     # MongoDB
     db_name = settings['mongodb.db_name']
     db_host = settings['mongodb.host']
