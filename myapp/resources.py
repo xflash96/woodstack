5ec47c10198bd4bdc22ff2fef23a3d7e999743ee
Po-Wei Wang <xflash96@gmail.com>
2012-03-03 03:34:40 +0800

    add db info

--- myapp/resources.py
+++ myapp/resources.py
@@ -4,6 +4,7 @@ class Root(object):
     def __init__(self, request):
         self.request = request
     def __getitem__(self, key):
+        return
         return Post.objects(key=key).first()
 
 class Post(Document):
