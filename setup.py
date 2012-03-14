9d56f5352d5adac83149031c73390b3da2a10c35
Po-Wei Wang <xflash96@gmail.com>
2012-03-14 16:19:25 +0800

    Change mongodb uri setting

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid', 'pyramid_debugtoolbar', 'mongoengine']
+requires = ['pyramid>=1.3', 'pyramid_debugtoolbar>=0.6.2', 'pymongo', 'gevent']
 
 setup(name='myapp',
       version='0.0',
